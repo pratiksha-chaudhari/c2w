5c1758e584ccbfefca225bc3106164bbb8f5fe9f
pratiksha <pratikshachaudhari154@gmail.com>
2024-07-01 13:45:15 +0000

    update

---
+++ c/dataType/compilationDemo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+
+	printf("pratiksha\n");
+	printf("keshav\n");
+
+}
+
---
+++ c/dataType/program1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+	int age=21;
+	float petPrice=105.65;
+	double gold=10.3405646;
+	char wing='A';
+
+	//void v;
+	
+	printf("%d\n",age);
+	printf("%f\n",petPrice);
+	printf("%f\n",gold);
+	printf("%c\n",wing);
+	printf("pratiksha\n");
+
+
+
+}
